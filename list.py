46dd1a3b262a2671c37985a40c3264a69d998729
danielliu <danielliu@DanieldeMacBook-Pro.local>
2019-02-15 17:40:41 +0800

    v1

---
+++ list.py
@@ -0,0 +1,10 @@
+#list 
+a = ['toyota', 'honda']
+print (a)
+print (a[1])
+print (a[0])
+a.append('audi')# append意思是list a 增加 a. 小数点代表“的“de shi 
+print (a)
+print (len(a))
+print ('audi' in a )
+print ('benz' in a )
